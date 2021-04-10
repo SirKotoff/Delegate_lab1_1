fbc209e526db3a621eac000b278a6fa7fb201016
SirKotoff <68810705+SirKotoff@users.noreply.github.com>
2021-04-10 21:58:40 +0300

    Добавьте файлы проекта.

---
+++ deleg_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deleg_2
+{
+    
+
+    class Program
+    {
+        public static float M()
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Action<Func<float>, bool, List<float>> A= null;
+            Func<float> func = null;
+            func += M;
+
+           
+
+            A += ((params1, params2, params3) =>
+
+             Console.WriteLine("Y")
+            
+
+            );
+
+
+            A?.Invoke(func, true,null);
+            
+           
+         
+        }
+
+
+ 
+
+        
+
+        
+
+    }
+}
